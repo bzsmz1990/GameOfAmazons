b8543547cb8cb23e93ee9ea0f60753ae1c4cb43a
hui-yang <hy821@nyu.edu>
2015-05-17 20:13:18 -0400

    instr z-idx

--- languages/en.js
+++ languages/en.js
@@ -1,6 +1,6 @@
 window.angularTranslationsLoaded('en', {
   AMAZONS_GAME: "Game of the Amazons",
-  RULES_OF_AMAZONS: "Rules of the Amazons",
+  RULES_OF_AMAZONS: "Rules of Amazons",
   RULES_SLIDE0: "Each player has four amazons, and a supply of arrows. White moves first, and the players alternate moves thereafter.",
   RULES_SLIDE1: "1. Move one amazon one or more empty squares in a straight line (orthogonally or diagonally).",
   RULES_SLIDE2: "2. The amazon shoots an arrow from its landing square to another square, using another queenlike move.",
